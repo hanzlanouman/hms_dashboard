dfdd17dc8d7d001b59d808a1192406bbfbbd64b0
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2023-04-10 09:24:46 +0500

    added different dashes for different users (student, cook, security)

--- src/main/java/com/hms/hms_dashboard_01/LoginController.java
+++ src/main/java/com/hms/hms_dashboard_01/LoginController.java
@@ -17,8 +17,14 @@ import java.util.Objects;
 
 public class LoginController {
 
-    private String User="admin";
-    private String Pass = "123";
+    private String adminUser="admin";
+    private String adminPass = "123";
+    private String studentUserName="student";
+    private String studentPassword = "123";
+    private String cookUser="cook";
+    private String cookPass = "123";
+    private String securityUser="security";
+    private String securityPass = "123";
 Stage stage;
 Scene scene;
 Parent root;
@@ -34,7 +40,7 @@ public void login(ActionEvent event) throws IOException {
     String username1 = username.getText();
     String password1 = password.getText();
 
-    if(Objects.equals(username1, User) && Objects.equals(password1, Pass)){
+    if(Objects.equals(username1, adminUser) && Objects.equals(password1, adminPass)){
         System.out.println(username1);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("hms_dash.fxml"));
         root = loader.load();
@@ -44,7 +50,39 @@ public void login(ActionEvent event) throws IOException {
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
-    }else{
+    }else if(Objects.equals(username1, studentUserName) && Objects.equals(password1, studentPassword)){
+        System.out.println(username1);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hms_dash_student.fxml"));
+        root = loader.load();
+        Controller controller = loader.getController();
+        controller.greet(username1);
+
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }else if(Objects.equals(username1, cookUser) && Objects.equals(password1, cookPass)){
+        System.out.println(username1);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hms_dash_cook.fxml"));
+        root = loader.load();
+        Controller controller = loader.getController();
+        controller.greet(username1);
+
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }else if(Objects.equals(username1, securityUser) && Objects.equals(password1, securityPass)){
+        System.out.println(username1);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hms_dash_security.fxml"));
+        root = loader.load();
+        Controller controller = loader.getController();
+        controller.greet(username1);
+
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    else{
         Alert loginAlert = new Alert(Alert.AlertType.ERROR);
         loginAlert.setTitle("Login Error");
         loginAlert.setHeaderText("Invalid Username or Password");
